391370d9dd7db6a6c9dd0df76ccbbe1e5726fdcd
jiaqinggang1 <forestjqg@126.com>
2021-03-24 22:12:09 +0800

    add test

---
+++ test/converterTest/claimMultiTimeInput.js
@@ -0,0 +1,175 @@
+const { time, expectEvent} = require("@openzeppelin/test-helpers")
+let utils = require('./utils.js');
+
+let CFNC = artifacts.require("CFNX");
+let TokenConverter = artifacts.require("TokenConverter");
+let TokenConverterProxy = artifacts.require("TokenConverterProxy");
+
+const BN = require("bn.js");
+const assert = require('assert');
+
+const ONE_HOUR = 60*60;
+const ONE_DAY = ONE_HOUR * 24;
+const ONE_MONTH = 30 * ONE_DAY;
+
+contract('TokenConverter', function (accounts) {
+    let cfnxAmount0 = new BN("30000000000000000000");
+    let cfnxAmount1 = new BN("60000000000000000000");
+    let cfnxAmount2 = new BN("120000000000000000000");
+    let cfnxAmount3 = new BN("125000000000000000000");
+    let fnxAmount = new BN("90000000000000000000000");
+
+    let CFNXInst;
+    let FNXInst;
+    let CvntInst;
+    let CvntProxyInst;
+    let STARTINPUTTIME;
+
+    before(async () => {
+        CFNXInst = await CFNC.new();
+        console.log("cfnx address:" + CFNXInst.address);
+
+        FNXInst = await CFNC.new();
+        console.log("fnx address:" + CFNXInst.address);
+
+        CvntInst = await TokenConverter.new();
+        console.log("converter address:" + CvntInst.address);
+
+        CvntProxyInst = await TokenConverterProxy.new(CvntInst.address);
+        console.log("proxy address:" + CvntProxyInst.address);
+
+        let tx = await CvntProxyInst.setParameter(CFNXInst.address,FNXInst.address,0,0,0);
+        assert.equal(tx.receipt.status,true);
+        ////init process
+        tx = await CFNXInst.mint(accounts[1],cfnxAmount1.mul(new BN(20)));
+        assert.equal(tx.receipt.status,true);
+
+        tx = await CFNXInst.mint(accounts[2],cfnxAmount2);
+        assert.equal(tx.receipt.status,true);
+
+        tx = await CFNXInst.mint(accounts[3],cfnxAmount3);
+        assert.equal(tx.receipt.status,true);
+
+        tx = await FNXInst.mint(CvntProxyInst.address,fnxAmount);
+        assert.equal(tx.receipt.status,true);
+
+        tx = await CFNXInst.approve(CvntProxyInst.address,cfnxAmount1.mul(new BN(20)),{from:accounts[1]});
+        assert.equal(tx.receipt.status,true);
+
+        tx = await CFNXInst.approve(CvntProxyInst.address,cfnxAmount2.mul(new BN(20)),{from:accounts[2]});
+        assert.equal(tx.receipt.status,true);
+
+        tx = await CFNXInst.approve(CvntProxyInst.address,cfnxAmount3.mul(new BN(20)),{from:accounts[3]});
+        assert.equal(tx.receipt.status,true);
+    });
+
+    it('1 User1 input CFNX with 100 tx and get 1/6 FNX ', async function () {
+        let beforeFnxUser =  await FNXInst.balanceOf(accounts[1]);
+        let beforeCFnxBalanceUser = await CFNXInst.balanceOf(accounts[1]);
+        console.log(beforeCFnxBalanceUser);
+        console.log(await CFNXInst.allowance(accounts[1],CvntProxyInst.address));
+
+        let beforeFnxBalanceProxy = await CFNXInst.balanceOf(CvntProxyInst.address);
+
+        for(var i=0;i<100;i++) {
+            tx = await CvntProxyInst.inputCfnxForInstallmentPay(cfnxAmount1.div(new BN(100)),{from:accounts[1]});
+            assert.equal(tx.receipt.status,true);
+            console.log(i);
+        }
+
+        STARTINPUTTIME = time.latest();
+        console.log("start time=" + STARTINPUTTIME)
+
+        let afterFnxUser =  await FNXInst.balanceOf(accounts[1]);
+        let afterCFnxBalanceUser = await CFNXInst.balanceOf(accounts[1]);
+        let afterFnxBalanceProxy = await CFNXInst.balanceOf(CvntProxyInst.address);
+
+        let diffCFNXUser = web3.utils.fromWei(new BN(beforeCFnxBalanceUser)) - web3.utils.fromWei(new BN(afterCFnxBalanceUser));
+        console.log("CFNX diff User:" + diffCFNXUser);
+        let diffContract = web3.utils.fromWei(new BN(afterFnxBalanceProxy)) - web3.utils.fromWei(new BN(beforeFnxBalanceProxy));
+        console.log("CFNX diff contract:" + diffContract);
+        assert.equal(diffCFNXUser,diffContract);
+
+        let diffFNXUser = web3.utils.fromWei(new BN(afterFnxUser)) - web3.utils.fromWei(new BN(beforeFnxUser));
+        console.log("FNX diff user:" + diffFNXUser);
+        assert.equal(diffFNXUser,diffContract/6);
+
+        let lockedBalance = await CvntProxyInst.lockedBalanceOf(accounts[1]);
+        console.log(web3.utils.fromWei(lockedBalance));
+        assert.equal(web3.utils.fromWei(lockedBalance),web3.utils.fromWei(cfnxAmount1)*5/6);
+
+    })
+
+
+    it('2 Get all FNX of the first batch input', async function () {
+        await time.increase(time.duration.days(30*5+1));
+        console.log("step 3 get out");
+
+        let beforeFnxUser =  await FNXInst.balanceOf(accounts[1]);
+        let tx = await CvntProxyInst.claimFnxExpiredReward({from:accounts[1]});
+        assert.equal(tx.receipt.status,true);
+        await utils.sleep(1000);
+        let afterFnxUser =  await FNXInst.balanceOf(accounts[1]);
+        let diffFNXUser = web3.utils.fromWei(new BN(afterFnxUser)) - web3.utils.fromWei(new BN(beforeFnxUser));
+        console.log("all FNX diff user:" + diffFNXUser);
+        assert.equal(diffFNXUser,web3.utils.fromWei(cfnxAmount1)*5/6);
+
+        let lockedBalance = await CvntProxyInst.lockedBalanceOf(accounts[1]);
+        assert.equal(web3.utils.fromWei(lockedBalance),0);
+    })
+
+    it('3 User1 input CFNX in 100 tx and get 1/6 FNX after one day', async function () {
+        await time.increase(time.duration.days(3));
+
+        let beforeFnxUser =  await FNXInst.balanceOf(accounts[1]);
+        let beforeCFnxBalanceUser = await CFNXInst.balanceOf(accounts[1]);
+        console.log(beforeCFnxBalanceUser);
+        console.log(await CFNXInst.allowance(accounts[1],CvntProxyInst.address));
+
+        let beforeFnxBalanceProxy = await CFNXInst.balanceOf(CvntProxyInst.address);
+
+        for(var i=0;i<200;i++) {
+            tx = await CvntProxyInst.inputCfnxForInstallmentPay(cfnxAmount1.div(new BN(200)),{from:accounts[1]});
+            assert.equal(tx.receipt.status,true);
+            console.log(i);
+        }
+
+        let afterFnxUser =  await FNXInst.balanceOf(accounts[1]);
+        let afterCFnxBalanceUser = await CFNXInst.balanceOf(accounts[1]);
+        let afterFnxBalanceProxy = await CFNXInst.balanceOf(CvntProxyInst.address);
+
+        let diffCFNXUser = web3.utils.fromWei(new BN(beforeCFnxBalanceUser)) - web3.utils.fromWei(new BN(afterCFnxBalanceUser));
+        console.log("CFNX diff User:" + diffCFNXUser);
+        let diffContract = web3.utils.fromWei(new BN(afterFnxBalanceProxy)) - web3.utils.fromWei(new BN(beforeFnxBalanceProxy));
+        console.log("CFNX diff contract:" + diffContract);
+        assert.equal(diffCFNXUser,diffContract);
+
+        let diffFNXUser = web3.utils.fromWei(new BN(afterFnxUser)) - web3.utils.fromWei(new BN(beforeFnxUser));
+        console.log("FNX diff user:" + diffFNXUser);
+        assert.equal(diffFNXUser,diffContract/6);
+
+        let lockedBalance = await CvntProxyInst.lockedBalanceOf(accounts[1]);
+        console.log(lockedBalance);
+        assert.equal(web3.utils.fromWei(lockedBalance),web3.utils.fromWei(cfnxAmount1)*5/6);
+
+    })
+
+
+    it('4 Get all FNX of the second batch input', async function () {
+        await time.increase(time.duration.days(30*5+1));
+        console.log("step 4 get out");
+
+        let beforeFnxUser =  await FNXInst.balanceOf(accounts[1]);
+        let tx = await CvntProxyInst.claimFnxExpiredReward({from:accounts[1]});
+        assert.equal(tx.receipt.status,true);
+        await utils.sleep(1000);
+        let afterFnxUser =  await FNXInst.balanceOf(accounts[1]);
+        let diffFNXUser = web3.utils.fromWei(new BN(afterFnxUser)) - web3.utils.fromWei(new BN(beforeFnxUser));
+        console.log("all FNX diff user:" + diffFNXUser);
+        assert.equal(diffFNXUser,web3.utils.fromWei(cfnxAmount1)*5/6);
+
+        let lockedBalance = await CvntProxyInst.lockedBalanceOf(accounts[1]);
+        assert.equal(web3.utils.fromWei(lockedBalance),0);
+    })
+
+})
